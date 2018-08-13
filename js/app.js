80460e7cc32b7b4fda9b4638003afe288c30a186
kris riedman <riedmank@gmail.com>
2018-08-13 15:07:01 -0700

    add plain text names to products

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 
 var userVotes = 0;
 
-function Product(fileName) {
+function Product(fileName, name) {
   this.fileName = fileName;
+  this.name = name;
   this.numVotes = 0;
   this.timesSeen = 0;
   Product.allProducts.push(this);
@@ -11,26 +12,26 @@ function Product(fileName) {
 
 Product.allProducts = [];
 
-var item1 = new Product('img/bag.jpg');
-var item2 = new Product('img/banana.jpg');
-var item3 = new Product('img/bathroom');
-new Product('img/boots.jpg');
-new Product('img/breakfast.jpg');
-new Product('img/bubblegum.jpg');
-new Product('img/chair.jpg');
-new Product('img/cthulhu.jpg');
-new Product('img/dog-duck.jpg');
-new Product('img/dragon.jpg');
-new Product('img/pen.jpg');
-new Product('img/pet-sweep.jpg');
-new Product('img/scissors.jpg');
-new Product('img/shark.jpg');
-new Product('img/sweep.png');
-new Product('img/tauntaun.jpg');
-new Product('img/unicorn.jpg');
-new Product('img/usb.gif');
-new Product('img/water-can.jpg');
-new Product('img/wine-glass.jpg');
+var item1 = new Product('img/bag.jpg', 'R2-D2 bag');
+var item2 = new Product('img/banana.jpg', 'Banana Slicer');
+var item3 = new Product('img/bathroom.jpg', 'Bathroom Tablet Stand');
+new Product('img/boots.jpg', 'Toeless Rain Boots');
+new Product('img/breakfast.jpg', 'All in one Breakfast Station');
+new Product('img/bubblegum.jpg', 'Meatball Bubblegum');
+new Product('img/chair.jpg', 'Ergonomically Incorrect Chair');
+new Product('img/cthulhu.jpg', 'Cthulhu Action Figure');
+new Product('img/dog-duck.jpg', 'Dog Duck Lips');
+new Product('img/dragon.jpg', 'Dragon Meat in a Can');
+new Product('img/pen.jpg', 'Pen Cap Utensils');
+new Product('img/pet-sweep.jpg', 'Pet Sweeper');
+new Product('img/scissors.jpg', 'Pizza Scissor');
+new Product('img/shark.jpg', 'Shark Sleeping Bag');
+new Product('img/sweep.png', 'Baby Sweeper');
+new Product('img/tauntaun.jpg', 'Tauntaun Sleeping Bag');
+new Product('img/unicorn.jpg', 'Unicorn Meat in a Can');
+new Product('img/usb.gif', 'Wiggling USB Tentacle');
+new Product('img/water-can.jpg', 'Self-Watering Water Can');
+new Product('img/wine-glass.jpg', 'Egg Wine Glass');
 
 item1.timesSeen++;
 item2.timesSeen++;
@@ -41,7 +42,7 @@ function displayNewProducts() {
   var randIndex2 = Math.floor(Math.random() * Product.allProducts.length);
   var randIndex3 = Math.floor(Math.random() * Product.allProducts.length);
   item1 = Product.allProducts[randIndex1];
-  while(randIndex1 === randIndex2 && randIndex1 === randIndex3 && randIndex2 === randIndex3) {
+  while(randIndex1 === randIndex2 || randIndex1 === randIndex3 || randIndex2 === randIndex3) {
     randIndex2 = Math.floor(Math.random() * Product.allProducts.length);
     randIndex3 = Math.floor(Math.random() * Product.allProducts.length);
   }
@@ -64,16 +65,19 @@ var radio3 = document.getElementById('vote3');
 var vote = document.getElementById('voteButton');
 
 vote.addEventListener('click', function() {
-  userVotes++;
-  if (radio1.checked) {
-    item1.numVotes++;
-    radio1.checked=false;
-  } else if (radio2.checked) {
-    item2.numVotes++;
-    radio2.checked=false;
-  } else {
-    item3.numVotes++;
-    radio3.checked=false;
+  while(userVotes < 25) {
+    userVotes++;
+    if (radio1.checked) {
+      item1.numVotes++;
+      radio1.checked=false;
+    } else if (radio2.checked) {
+      item2.numVotes++;
+      radio2.checked=false;
+    } else {
+      item3.numVotes++;
+      radio3.checked=false;
+    }
+    displayNewProducts();
   }
-  displayNewProducts();
+  getResults();
 });
