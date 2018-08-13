1b8c147d2229e7dc9227fbaaad35f3be6062f63c
kris riedman <riedmank@gmail.com>
2018-08-13 14:10:35 -0700

    add js stuff, need to test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,64 @@
 'use strict';
+
+function Product(fileName) {
+  this.fileName = fileName;
+  this.numVotes = 0;
+  this.timesSeen = 0;
+  Product.allProducts.push(this);
+}
+
+Product.allProducts = [];
+
+var item1 = new Product('img/bag.jpg');
+var item2 = new Product('img/banana.jpg');
+var item3 = new Product('img/bathroom');
+new Product('img/boots.jpg');
+new Product('img/breakfast.jpg');
+new Product('img/bubblegum.jpg');
+new Product('img/chair.jpg');
+new Product('img/cthulhu.jpg');
+new Product('img/dog-duck.jpg');
+new Product('img/dragon.jpg');
+new Product('img/pen.jpg');
+new Product('img/pet-sweep.jpg');
+new Product('img/scissors.jpg');
+new Product('img/shark.jpg');
+new Product('img/sweep.png');
+new Product('img/tauntaun.jpg');
+new Product('img/unicorn.jpg');
+new Product('img/usb.gif');
+new Product('img/water-can.jpg');
+new Product('img/wine-glass.jpg');
+
+function displayNewProducts() {
+  var randIndex1 = Math.floor(Math.random() * Product.allProducts.length);
+  var randIndex2 = Math.floor(Math.random() * Product.allProducts.length);
+  var randIndex3 = Math.floor(Math.random() * Product.allProducts.length);
+  item1 = Product.allProducts[randIndex1];
+  while(randIndex1 === randIndex2 && randIndex1 === randIndex3 && randIndex2 === randIndex3) {
+    randIndex2 = Math.floor(Math.random() * Product.allProducts.length);
+    randIndex3 = Math.floor(Math.random() * Product.allProducts.length);
+  }
+  item2 = Product.allProducts[randIndex2];
+  item3 = Product.allProducts[randIndex3];
+  prod1.src = item1.fileName;
+  prod2.src = item2.fileName;
+  prod3.src = item3.filename;
+}
+
+var prod1 = document.getElementsByTagName('img')[0];
+var prod2 = document.getElementsByTagName('img')[1];
+var prod3 = document.getElementsByTagName('img')[2];
+var vote = document.getElementById('voteButton');
+
+vote.addEventListener('click', function() {
+  if (document.getElementById('vote1').checked) {
+    item1.numVotes++;
+  } else if (document.getElementById('vote2').checked) {
+    item2.numVotes++;
+  } else {
+    item3.numVotes++;
+  }
+  displayNewProducts();
+});
+
